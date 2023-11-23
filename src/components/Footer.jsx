d351de3e9459237a3d6a5eafcce7989b1057d86c
Manasvinitroshan <manas.comet.25@gmail.com>
2023-11-22 20:48:19 -0600

    Footer added, insta icon links to instagram

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,17 +1,32 @@
-import React from 'react'
-
+import React from 'react';
 import {
-    FaDribbbleSquare,
-    FaFacebookSquare,
-    FaGithubSquare,
-    FaInstagram,
-    FaTwitterSquare,
-  } from 'react-icons/fa';
+  FaDribbbleSquare,
+  FaFacebookSquare,
+  FaGithubSquare,
+  FaInstagram,
+  FaTwitterSquare,
+} from 'react-icons/fa';
 
 const Footer = () => {
+     const instagramPageUrl = 'https://www.instagram.com/thetatauaplha/'
   return (
-    <div>Footer</div>
-  )
-}
+    <div className='max-w-[1000px] mx-auto py-16 px-4 grid lg:grid-cols-1 gap-8 text-gray-300'>
+      <div>
+        <h1 className='w-full text-3xl font-bold text-[white]'>ΘTA.</h1>
+        <p className='py-3'>© THETA TAU ALPHA ALPHA COLONY ALL RIGHTS RESERVED</p>
+        <div className='flex justify-between md:w-[75%] my-6'>
+            <FaFacebookSquare size={30} />
+            <a href={instagramPageUrl} target="_blank" rel="noopener noreferrer">
+      <FaInstagram size={30} />
+    </a>
+            <FaTwitterSquare size={30} />
+            <FaGithubSquare size={30} />
+            <FaDribbbleSquare size={30} />
+        </div>
+      </div>
+      
+    </div>
+  );
+};
 
-export default Footer
+export default Footer;
--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import LandingPage from '/Users/manassingh/Desktop/TTA-Website/ttaweb/src/LandingPage.jsx'
+import Footer from './Footer'
 
 const Home = () => {
   return (
     <div>
 
         <LandingPage/>
+        <Footer/>
     </div>
   )
 }
